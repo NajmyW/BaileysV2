e5683a1002a9a9e3a742b1a7836a1923ac84f26c
X-Tio <satriopambudi866@gmail.com>
2025-07-06 21:11:05 +0700

    update messages.ts

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -28,7 +28,7 @@ import { sha256 } from './crypto'
 import { generateMessageIDV2, getKeyAuthor, unixTimestampSeconds } from './generics'
 import { ILogger } from './logger'
 import { downloadContentFromMessage, encryptedStream, generateThumbnail, getAudioDuration, getAudioWaveform, MediaDownloadOptions } from './messages-media'
-
+const ButtonType = proto.Message.ButtonsMessage.HeaderType
 type MediaUploadData = {
 	media: WAMediaUpload
 	caption?: string
