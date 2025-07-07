1be8ad25d6b125f783b54b18e9119da69c643a98
X-Tio <satriopambudi866@gmail.com>
2025-07-07 07:23:20 +0700

    update messages-send.ts

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -3,7 +3,7 @@ import NodeCache from '@cacheable/node-cache'
 import { Boom } from '@hapi/boom'
 import { proto } from '../../WAProto'
 import { DEFAULT_CACHE_TTLS, WA_DEFAULT_EPHEMERAL } from '../Defaults'
-import { AnyMessageContent, MediaConnInfo, MessageReceiptType, MessageRelayOptions, MiscMessageGenerationOptions, WAMediaUploadFunctionOpts, SocketConfig, WAMessageKey } from '../Types'
+import { AnyMessageContent, MediaConnInfo, MessageReceiptType, MessageRelayOptions, MiscMessageGenerationOptions, WAMediaUploadFunction, SocketConfig, WAMessageKey } from '../Types'
 import { aggregateMessageKeysNotFromMe, assertMediaContent, bindWaitForEvent, decryptMediaRetryData, encodeSignedDeviceIdentity, encodeWAMessage, encryptMediaRetryRequest, extractDeviceJids, generateMessageIDV2, generateWAMessage, getStatusCodeForMediaRetry, getUrlFromDirectPath, getWAUploadToServer, normalizeMessageContent, parseAndInjectE2ESessions, getContentType, unixTimestampSeconds, delay } from '../Utils'
 import { getUrlInfo } from '../Utils/link-preview'
 import { areJidsSameUser, BinaryNode, BinaryNodeAttributes, getBinaryNodeChild, getBinaryNodeChildren, isJidGroup, isJidUser, jidDecode, jidEncode, jidNormalizedUser, JidWithDevice, S_WHATSAPP_NET, STORIES_JID } from '../WABinary'
@@ -836,7 +836,7 @@ const filterNativeNode = (nodeContent) => {
 							: undefined
 						},
 				   ),
-				   upload: async(readStream: Readable, opts: WAMediaUploadFunctionOpts) => {
+				   upload: async(readStream: Readable, opts: WAMediaUploadFunction) => {
 						const up = await waUploadToServer(readStream, { ...opts })
 					    mediaHandle = up.handle
 					    return up
